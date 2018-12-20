b532c96bd4c2de55276abcf69c6a1cefa1eec02c
PetarCetkovic <pcetkovic007@gmail.com>
2018-12-20 19:09:49 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+
+const express = require("express");
+const hbs = require("hbs");
+
+const app = express();
+hbs.registerPartials(__dirname+"/views/partials");
+app.set("view engine", "hbs");
+
+hbs.registerHelper("currentYear",()=>{
+    return (new Date()).getFullYear();
+})
+
+hbs.registerHelper("toUpper", (text)=>{
+    return text.toUpperCase();
+})
+
+app.use((req,res,next)=>{
+    res.render("maintanance.hbs");
+    next();
+})
+
+app.get("/",(req,res)=>{
+    res.render("home.hbs", {
+        pageTitle:"Home Page",
+        welcomeMessage: "Welcome to Home page"
+    })
+})
+
+app.get("/about",(req,res)=>{
+    const aboutPageMessage = "About Page";
+    res.render("about.hbs", {
+        pageTitle:aboutPageMessage,
+        welcomeMessage: `Welcome to ${aboutPageMessage}`
+    })
+})
+
+app.get("/help",(req,res)=>{
+    const helpPageMessage = "Help Page";
+    res.render("about.hbs", {
+        pageTitle:helpPageMessage,
+        welcomeMessage: `Welcome to ${helpPageMessage}`
+    })
+})
+
+app.listen(3000);
